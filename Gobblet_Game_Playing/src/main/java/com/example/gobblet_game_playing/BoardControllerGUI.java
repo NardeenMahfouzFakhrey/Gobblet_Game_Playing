311191f1b53495695c8620682acbe26cec9f7141
NardeenMahfouzFakhrey <1900586@eng.asu.edu.eg>
2024-01-27 19:50:13 +0200

    Fix Gobblet Gui

--- Gobblet_Game_Playing/src/main/java/com/example/gobblet_game_playing/BoardControllerGUI.java
+++ Gobblet_Game_Playing/src/main/java/com/example/gobblet_game_playing/BoardControllerGUI.java
@@ -177,6 +177,8 @@ public class BoardControllerGUI {
 
     public static void switchTurnHandleGobblets() {
         if (BoardGUI.game.currentTurn == Game.Turn.A) {
+            BoardGUI.whiteBox.setDisable(true);
+            BoardGUI.blackBox.setDisable(false);
             for (int x = 0; x < 4; x++) {
                 for (int y = 0; y < 4; y++) {
                     for (int l = BoardGUI.buttonPanes[x][y].getChildren().size() - 1; l >= 0; l--) {
@@ -184,17 +186,16 @@ public class BoardControllerGUI {
                         if (!(BoardGUI.game.getBoard().getFront(x, y) == null) && BoardGUI.game.getBoard().getFront(x, y).getGobbletColor() == GobbletColor.BLACK) {
                             if (lastChild != null) {
                                 lastChild.setDisable(true);
-                                BoardGUI.blackBox.setDisable(true);
                             }
-
                         } else {
                             lastChild.setDisable(false);
-                            BoardGUI.blackBox.setDisable(false);
                         }
                     }
                 }
             }
         } else if (BoardGUI.game.currentTurn == Game.Turn.B) {
+            BoardGUI.whiteBox.setDisable(false);
+            BoardGUI.blackBox.setDisable(true);
             for (int x = 0; x < 4; x++) {
                 for (int y = 0; y < 4; y++) {
 
@@ -203,11 +204,11 @@ public class BoardControllerGUI {
                         if (!(BoardGUI.game.getBoard().getFront(x, y) == null) && BoardGUI.game.getBoard().getFront(x, y).getGobbletColor() == GobbletColor.WHITE) {
                             if (lastChild != null) {
                                 lastChild.setDisable(true);
-                                BoardGUI.whiteBox.setDisable(true);
+
                             }
 
                         } else {
-                            BoardGUI.whiteBox.setDisable(false);
+
                             lastChild.setDisable(false);
                         }
                     }
